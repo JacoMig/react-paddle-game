3c2fc420b8f875db7f51671d821db9ba9e14bba0
Jacopo <jacomig@gmail.com>
2020-06-19 15:27:31 +0200

    Add webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,83 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-module.exports = {
-  devtool : 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|png|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-           /*  options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            } */
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = env => {
+  const mode = env && env.NODE_ENV ? env.NODE_ENV : ""
+  
+  return {
+    devtool : mode === 'production' ? false : 'inline-source-map',
+    entry: {
+      app: './src/index.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-        test: /\.svg/,
-        use: {
-            loader: 'svg-url-loader',
-            options: {}
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|png|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+            /*  options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+              } */
+            }
+          ]
+        },
+        {
+          test: /\.svg/,
+          use: {
+              loader: 'svg-url-loader',
+              options: {}
           }
-        ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
       }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
-};
+    }
+  }
+}
